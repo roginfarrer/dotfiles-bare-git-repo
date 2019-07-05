f956358d94986cf06c91b03caf33158e92acfb09
Rogin Farrer <rfarrer@wayfair.com>
2019-07-05 14:53:12 -0400

    remove spaceship exec duplication

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -32,6 +32,3 @@ export FZF_DEFAULT_COMMAND='rg --files --follow --hidden -g "!node_modules"'
 
 export PATH="/Users/rfarrer/.nvm/versions/node/v8.9.4/bin:/Users/rfarrer/.local/bin:”/Users/rfarrer/platform-tools/bin:/usr/local/opt/php@7.2/sbin:/usr/local/opt/php@7.2/bin:/Users/rfarrer/.composer/vendor/bin:/Users/rfarrer/Wayfair/wf:/Users/rfarrer/.rbenv/bin:/usr/local/share/npm/bin:/usr/local/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Users/rfarrer/bin:/usr/local/bin”:/Users/rfarrer/bin:/usr/local/opt/fzf/bin:/Users/rfarrer/.vimpkg/bin"
 
-  # Set Spaceship ZSH as a prompt
-  autoload -U promptinit; promptinit
-  prompt spaceship
