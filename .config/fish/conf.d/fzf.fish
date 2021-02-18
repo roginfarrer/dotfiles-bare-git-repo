df1bba89f4727e4ccdef5799622287248d08536d
Rogin Farrer <rogin@roginfarrer.com>
2021-02-18 10:30:35 -0500

    maybe fix fish files ?

--- .config/fish/conf.d/fzf.fish
+++ .config/fish/conf.d/fzf.fish
@@ -2,14 +2,14 @@
 # them before even executing __fzf_search_shell_variables. We use psub to store the
 # variables' info in temporary files and pass in the filenames as arguments.
 # This variable is intentionally global so that it can be referenced by custom key bindings and tests
-set __fzf_search_vars_cmd '__fzf_search_shell_variables (set --show | psub) (set --names | psub)'
+set --global fzf_search_vars_cmd '__fzf_search_shell_variables (set --show | psub) (set --names | psub)'
 
 # Set up the default, mnemonic key bindings unless the user has chosen to customize them
 if not set --query fzf_fish_custom_keybindings
     # \cf is Ctrl+f
     bind \cf '__fzf_search_current_dir'
     bind \cr '__fzf_search_history'
-    bind \cv $__fzf_search_vars_cmd
+    bind \cv $fzf_search_vars_cmd
     # The following two key binding use Alt as an additional modifier key to avoid conflicts
     bind \e\cl '__fzf_search_git_log'
     bind \e\cs '__fzf_search_git_status'
@@ -18,7 +18,7 @@ if not set --query fzf_fish_custom_keybindings
     if test "$fish_key_bindings" = 'fish_vi_key_bindings'
         bind --mode insert \cf '__fzf_search_current_dir'
         bind --mode insert \cr '__fzf_search_history'
-        bind --mode insert \cv $__fzf_search_vars_cmd
+        bind --mode insert \cv $fzf_search_vars_cmd
         bind --mode insert \e\cl '__fzf_search_git_log'
         bind --mode insert \e\cs '__fzf_search_git_status'
     end
@@ -33,7 +33,7 @@ if not set --query FZF_DEFAULT_OPTS
     # height=90% leaves space to see the current command and some scrollback, maintaining context of work
     # preview-window=wrap wraps long lines in the preview window, making reading easier
     # marker=* makes the multi-select marker more distinguishable from the pointer (since both default to >)
-    set --export FZF_DEFAULT_OPTS '--cycle --layout=reverse --border --height=90% --preview-window=wrap --marker="*"'
+    set --global --export FZF_DEFAULT_OPTS '--cycle --layout=reverse --border --height=90% --preview-window=wrap --marker="*"'
 end
 
 function _fzf_uninstall --on-event fzf_uninstall
@@ -49,5 +49,7 @@ function _fzf_uninstall --on-event fzf_uninstall
         echo "fzf.fish key bindings removed"
         set_color normal
     end
+
+    set --erase __fzf_search_vars_cmd
     functions --erase _fzf_uninstall
 end
